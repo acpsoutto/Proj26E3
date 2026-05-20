3381df3eee4b9598e184e29dd5f2d5eeaada9e3c
Mathias <Mathias@10.179.24.199>
2026-05-20 17:10:57 +0100

    Creacion de la Classe Administrado y constructor del mismo

---
+++ projE3/src/projE3/Administrador.java
@@ -0,0 +1,15 @@
+package projE3;
+
+public class Administrador extends Funcionario{
+
+	/**
+	 * @param numeroTrabalhador
+	 * @param nome
+	 * @param email
+	 * @param codigoAcesso
+	 */
+	public Administrador(int numeroTrabalhador, String nome, String email, String codigoAcesso) {
+		super(numeroTrabalhador, nome, email, codigoAcesso);
+	}
+	
+}

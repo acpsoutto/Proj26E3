da0acc5c7a1b88202e986506e0a65e04f75e0bce
mathy1759 <mathy1759@gmail.com>
2026-05-21 09:57:35 +0100

    [ADD] Administrador

--- src/proj26E3/Administrador.java
+++ src/proj26E3/Administrador.java
@@ -1,9 +1,9 @@
 package proj26E3;
+import java.util.Scanner;
 
 public class Administrador extends Funcionario{
 
 	/**
-	 * CONSTRUTOR
 	 * @param numeroTrabalhador
 	 * @param nome
 	 * @param email
@@ -13,6 +13,30 @@ public class Administrador extends Funcionario{
 		super(numeroTrabalhador, nome, email, codigoAcesso);
 	}
 	
-	public void registarFuncionario(Funcionario f) {}
+	public void registarFuncionario(GerirBar gerir, Scanner sc) {
+		System.out.println("Insira o tipo de funcionário:\n1-Gerente\n2-Vendedor");
+		int op=sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("Insira o numero do Trabalhador: ");
+		int nTra=sc.nextInt();
+		sc.nextLine();
+		System.out.println("Insira o nome: ");
+		String nome=sc.nextLine();
+		System.out.println("Insira o email: ");
+		String mail=sc.nextLine();
+		System.out.println("Insira o codigo de acceso: ");
+		String codAcc=sc.nextLine();
+		
+		switch (op) {
+			case 1:
+				gerir.addFuncionario(new Gerente(nTra,nome,mail,codAcc));
+				break;
+			case 2:
+				/**
+				 gerir.addFuncionario(new FuncionarioBar(nTra,nome,mail,codAcc)); 
+				 * */
+		}
+	}
 	
 }

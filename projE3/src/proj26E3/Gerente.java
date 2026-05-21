f802d3ec7dfc3ce35bf1beb6ab2794837844bfe9
João Pinto <jgpinto2003@gmail.com>
2026-05-21 09:45:14 +0100

    [DEL] projE3 diretório
    erro com os diretórios

--- projE3/src/proj26E3/Gerente.java
+++
@@ -1,8 +0,0 @@
-package proj26E3;
-
-public class Gerente extends Funcionario {
-
-	public Gerente(int numeroTrabalhador,String nome,String email, String codigoAcesso) {
-		super(numeroTrabalhador,nome,email,codigoAcesso );
-	}
-}
